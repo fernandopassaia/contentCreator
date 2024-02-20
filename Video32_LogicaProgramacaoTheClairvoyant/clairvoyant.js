965d09f651da6d21e37650169b437b95eee5be02
Fernando Passaia <fernandopassaia@futuradata.com.br>
2024-02-20 21:20:27 +0000

    Before implementing Mission Impossible

--- Video32_LogicaProgramacaoTheClairvoyant/clairvoyant.js
+++ Video32_LogicaProgramacaoTheClairvoyant/clairvoyant.js
@@ -1,13 +1,12 @@
 window.onload = function() {
-console.log("Cheguei1");
 const playBTN = document.getElementById("play-btn");
 const synth = new Tone.Synth().toDestination();
-console.log("document", document);
-console.log("Cheguei2", playBTN);
 playBTN.addEventListener("click", () => {
     if (Tone.context.state !== "running") {
       Tone.start();
     }
     synth.triggerAttackRelease("C4", "4n");
   });
+
+
 }
