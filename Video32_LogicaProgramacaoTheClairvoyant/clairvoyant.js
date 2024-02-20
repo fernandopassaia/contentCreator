ec6bdb7fc0aa6dedc082771605ca5682c9027700
Fernando Passaia <fernandopassaia@futuradata.com.br>
2024-02-20 21:31:26 +0000

    adding mission impossible to the thing

--- Video32_LogicaProgramacaoTheClairvoyant/clairvoyant.js
+++ Video32_LogicaProgramacaoTheClairvoyant/clairvoyant.js
@@ -1,5 +1,6 @@
 window.onload = function() {
 const playBTN = document.getElementById("play-btn");
+const playBTN2 = document.getElementById("play-btn2");
 const synth = new Tone.Synth().toDestination();
 playBTN.addEventListener("click", () => {
     if (Tone.context.state !== "running") {
@@ -8,5 +9,37 @@ playBTN.addEventListener("click", () => {
     synth.triggerAttackRelease("C4", "4n");
   });
 
+  playBTN2.addEventListener("click", () => {
+    if (Tone.context.state !== "running") {
+      Tone.start();
+    }
+
+    const synth = new Tone.MembraneSynth().toMaster();
+  const notes = [
+    "G2",
+    [null, "G2"],
+    null,
+    "Bb2",
+    "C3",
+    "G2",
+    [null, "G2"],
+    null,
+    "F2",
+    "F#2"
+  ];
+
+  const synthPart = new Tone.Sequence(
+    function(time, note) {
+      synth.triggerAttackRelease(note, "10hz", time);
+    },
+    notes,
+    "8n"
+  );
+
+  synthPart.start();
+  Tone.Transport.start();
+
+
+  });
 
 }
