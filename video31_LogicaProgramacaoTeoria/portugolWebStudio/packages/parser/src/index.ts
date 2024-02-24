da4b52c356b4a52b87fcd506ac0eb572261805a7
Fernando Passaia <fernandopassaia@futuradata.com.br>
2024-02-24 08:05:50 +0000

    Reorganising the folder

--- video31_LogicaProgramacaoTeoria/portugolWebStudio/packages/parser/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./helpers/ParseError.js";
-export * from "./helpers/Tipo.js";
-export * from "./nodes/index.js";
-export * from "./PortugolNode.js";
-export * from "./PortugolErrorChecker.js";
--- video31_LogicaProgramacaoTeoria/portugolWebStudio/packages/runtime/src/runtime/index.ts
+++
@@ -1,9 +0,0 @@
-import { portugolRuntime } from "./PortugolRuntime.js";
-import { portugolVar } from "./PortugolVar.js";
-
-export const runtime = /* javascript */ `
-//region Runtime
-${portugolVar}
-${portugolRuntime}
-//endregion
-`;
